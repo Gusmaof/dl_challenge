cc1ece7b9f23a958d0a8a7a9bab29fedc3e3d8d4
Gusmaoguilherme <gusmao.f.guilherme@gmail.com>
2021-11-03 22:18:39 -0300

    Revert "math"
    This reverts commit efd4f08d5599d1c45d90432bfae34a931c01e60b.

--- deep_equation/src/deep_equation/predictor.py
+++ deep_equation/src/deep_equation/predictor.py
@@ -6,50 +6,11 @@ from typing import List
 import numpy as np
 
 
-# make a prediction for a new image.
-from tensorflow.keras.preprocessing.image import load_img
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.models import load_model
- 
-## load and prepare the image
-#def load_image(filename):
-#	# load the image
-#	print(filename)
-#	img = load_img(filename, grayscale=True, target_size=(28, 28))
-#	plt.imshow(img)
-#	plt.show()
-#	# convert to array
-#	img = img_to_array(img)
-#	# reshape into a single sample with 1 channel
-#	img = img.reshape(1, 28, 28, 1)
-#	# prepare pixel data
-#	img = img.astype('float32')
-#	img = img / 255.0
-#	return img
- 
-## load an image and predict the class
-#def run_example():
-#	# load the image
-#	img = load_image('sample_image.png')
-#	img2 = load_image('sample_image2.png')
-#	# load model
-#	model = load_model('trained_model/final_model.h5')
-#	# predict the class
-#	digitA,digitB = model.predict([img,img2])
-#	Amax=np.argmax(digitA)
-#	print(Amax)
-#	Bmax=np.argmax(digitB)
-#	print(Bmax)
- 
-# entry point, run the example
-#run_example()
-
 class BaseNet:
     """
     Base class that must be used as base interface to implement 
     the predictor using the model trained by the student.
     """
-
 
     def load_model(self, model_path):
         """
@@ -116,106 +77,24 @@ class StudentModel(BaseNet):
         load_model: method that loads your best model.
         predict: method that makes batch predictions.
     """
-    def operation(i):
-        switcher={
-                '+':1,
-                '-':2,
-                '*':3,
-                '/':4,
-             }
-        return switcher.get(i,"Invalid operator")
 
-    def calculator(DigitA,DigitB,operator):
-        
-        O=StudentModel.operation(operator[1])
-        
-        if O==1: 
-            result=DigitA+DigitB
-        elif O==2:
-            result=DigitA-DigitB
-        elif O==3:
-            result=DigitA*DigitB
-        elif (O==4) & (DigitB != 0):
-            result=DigitA/DigitB
-        else:
-            result = 'ERROR'
-
-        return result
-
-    # load and prepare the image
-    def load_image(filename: str):
-	    # load the image
-        img = load_img(filename, grayscale=True, target_size=(28, 28))
-        plt.imshow(img)
-        #plt.show()
-        # convert to array
-        img = img_to_array(img)
-        # reshape into a single sample with 1 channel
-        img = img.reshape(1, 28, 28, 1)
-        # prepare pixel data
-        img = img.astype('float32')
-        img = img / 255.0
-        return img
-
-    def load_model(self,model_path: str):
+    # TODO
+    def load_model(self, model_path: str):
         """
         Load the student's trained model.
         TODO: update the default `model_path` 
               to be the correct path for your best model!
         """
-        # load model
-        model = load_model(model_path)
-        return model 
-        
+        pass
+    
     # TODO:
     def predict(
-        images_a, images_b,
-        operators, device = 'cuda'
-    )-> List[str]:
-
+        self, images_a, images_b,
+        operators, device = 'cpu'
+    ):
         """Implement this method to perform predictions 
         given a list of images_a, images_b and operators.
         """
-
-        # load the image
-        #img = StudentModel.load_image('deep_equation/resources/trained_model/sample_image.png')
-        #img2 = StudentModel.load_image('deep_equation/resources/trained_model/sample_image2.png')
-        # load model
-        model = load_model('deep_equation/resources/trained_model/final_model.h5')
-        # predict the class
         predictions = []
-
-        for image_a, image_b, operator in zip(images_a, images_b, operators):            
-            img = StudentModel.load_image(image_a[1:-1])
-            img2 = StudentModel.load_image(image_b[1:-1])
-            digitA,digitB = model.predict([img,img2])
-            Amax=np.argmax(digitA)
-            print(Amax)
-            Bmax=np.argmax(digitB)
-            print(Bmax)
-            result=StudentModel.calculator(Amax,Bmax,operator)
-            predictions.append(result)
-
         
         return predictions
- 
-#Run
-
-
-with open('deep_equation/resources/data/inputs.txt') as f:
-    lines = [line.rstrip() for line in f]
-
-images_a=[]
-images_b=[]
-operators=[]
-
-for i in range(len(lines)):
-    x = lines[i]
-    result = x.split(',')
-    
-    images_a.append(result[0])
-    images_b.append(result[1])
-    operators.append(result[2])
-
-predictions=StudentModel.predict(images_a,images_b,operators,device='cuda')
-print(predictions)
